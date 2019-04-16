9c4d27f4c9cf135e2e8fe985c8277f69f8f44bd8
lustoo <1272643736@qq.com>
2019-04-16 14:38:27 +0800

    over

--- src/reVG.java
+++ src/reVG.java
@@ -7,19 +7,20 @@ import java.util.Map;
 public class reVG {
     private JPanel revgpanel;
     private JLabel reco;
-    private JTextField recovertext;
     private JButton 解密Button;
     private JLabel yw;
     private JTextField encryptext;
+    private JTextArea recovertext;
 
     public reVG() {
         解密Button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                //统计概率
                 double[] probability = new double[]{0.082, 0.015, 0.028, 0.043, 0.127, 0.022, 0.02, 0.061, 0.07, 0.002, 0.008,
                         0.04, 0.024, 0.067, 0.075, 0.019, 0.001, 0.06, 0.063, 0.091, 0.028, 0.01, 0.023, 0.001, 0.02, 0.001};
                 double M[] = new double[26];
-                double f[] = new double[26];
+                double f[] = new double[26];//出现频率(次数)
                 String encrypted=encryptext.getText().toLowerCase().replace(" ","");
                 System.out.println(encrypted.length());
                 int l=encrypted.length();
@@ -58,9 +59,7 @@ public class reVG {
                     }
                 }
                 System.out.println("分组密钥长度"+ansn);
-                //System.out.println("分组长度为"+l/ansn);
                 String blocks[] = new String[200];
-
                 for(int j=0;j<ansn;j++) {//按ans分组
                     int temp=l/ansn;
                     if(ansn*temp!=l)temp++;
@@ -69,10 +68,6 @@ public class reVG {
                     {
                         if(k*ansn+j<l) {
                             blocks[j] += encrypted.charAt(k*ansn+j);
-//                            if (map.containsKey(encrypted.charAt(k * ansn + j)))
-//                                map.put(encrypted.charAt(k * ansn + j), map.get(encrypted.charAt(k * ansn + j)) + 1);
-//                            else
-//                                map.put(encrypted.charAt(k * ansn + j), 1);
                         }
                     }
                     System.out.println(blocks[j]);
@@ -80,7 +75,7 @@ public class reVG {
 
                 //统计密文中所有字母的概率
                 String anskey="";
-                double close=0.0655;
+                double close;
                 char ansc='0';
                 double sump=0;
                 for(int t=0;t<ansn;++t) {//对于每个block
@@ -89,8 +84,6 @@ public class reVG {
                     Map<Character,Integer> map=new HashMap<>();
                     //blocks[t]中进行统计
                     int len=blocks[t].length();
-                    //System.out.println("len"+len);
-
 
                     for(int i=0;i<len;++i){//加到map
                         if (map.containsKey(blocks[t].charAt(i)))
@@ -98,7 +91,6 @@ public class reVG {
                             else
                                 map.put(blocks[t].charAt(i), 1);
                     }
-
                     for (int i = 0; i < 26; ++i) {
                         if (!map.containsKey((char) ('a' + i)))
                         {
@@ -109,10 +101,6 @@ public class reVG {
                             f[i] = 1.0*map.get((char)('a' + i)) / blocks[t].length();
                         }
                     }
-
-                    //System.out.println("f26-----------");
-                   // for(int i=0;i<1;++i)
-                        //System.out.println((char)('a'+i)+" "+f[i]);
                     for (int i = 0; i < 26; ++i) {//移位数//////////////////
                         double sum = 0;
                         for (int j = 0; j < 26; ++j) {
@@ -123,14 +111,27 @@ public class reVG {
                             close=M[i];
                             ansc= (char) ('a'+i);
                         }
-                        System.out.println((char)(i+'a')+" "+M[i]+" "+close);
+                        //System.out.println((char)(i+'a')+" "+M[i]);
                     }
                     sump+=close;
                     anskey+=ansc;
-                    //System.out.println(close);
                 }
                 System.out.println(anskey+" "+sump/ansn);
-
+                String keys=anskey;
+                while(keys.length()<l)
+                    keys+=anskey;
+                StringBuilder recover = new StringBuilder(encrypted);
+                for(int i=0;i<l;++i){
+                    int ttp=(encrypted.charAt(i)-(keys.charAt(i)-'a'));
+                    recover.replace(i,i+1, String.valueOf((char)(ttp<'a'?ttp+26:ttp)));
+                }
+                for(int i=0;i<l;++i){
+                    if(i!=0&&i%30==0){
+                        recover=recover.insert(i,'\n');
+                    }
+                }
+                System.out.println(recover);
+                recovertext.setText(String.valueOf(recover));
             }
         });
     }
@@ -141,16 +142,11 @@ public class reVG {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
-        frame.setSize(520,450);
+        frame.setSize(620,550);
         frame.setLocationRelativeTo(null);
     }
     static double cal(double fz,double fm){
         double temp=(fz-1)*fz/(fm*(fm-1));
         return temp;
-    }
-    static double abs(double a){
-        if(a<0)return -a;
-        else return a;
-
     }
 }
