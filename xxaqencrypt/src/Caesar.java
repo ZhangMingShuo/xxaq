29a4ac2c5ea92d68b881004b3c2dd9ce08c9d5b5
lustoo <1272643736@qq.com>
2019-05-01 15:51:44 +0800

    aes

---
+++ xxaqencrypt/src/Caesar.java
@@ -0,0 +1,46 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Caesar {
+    private JPanel mainpanel;
+    private JButton 加密Button;
+    private JTextField encrypttext;
+    private JTextField yuan;
+    private JTextField textn;
+    private JLabel yw;
+
+    public Caesar() {
+        加密Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int n=Integer.parseInt(textn.getText());
+                String origin = yuan.getText();
+                origin=origin.toLowerCase();
+                origin=origin.replace(" ","");
+                int l=origin.length();
+                StringBuilder encrypted=new StringBuilder(origin);
+                char c;
+                for(int i=0;i<l;++i)
+                {
+                    c=origin.charAt(i);
+                    c= (char) (c+n);
+                    if(c>'z')c= (char) (c-26);
+                    encrypted.replace(i,i+1, String.valueOf(c));
+                }
+                encrypttext.setText(String.valueOf(encrypted));
+            }
+        });
+    }
+
+    public static void main(String[] args) throws ClassNotFoundException, UnsupportedLookAndFeelException, InstantiationException, IllegalAccessException {
+        UIManager.setLookAndFeel(javax.swing.plaf.nimbus.NimbusLookAndFeel.class.getName());
+        JFrame frame = new JFrame("古典加密算法_1617440518");
+        frame.setContentPane(new Caesar().mainpanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+        frame.setSize(520,450);
+        frame.setLocationRelativeTo(null);
+    }
+}
---
+++ xxaqencrypt/src/VG.java
@@ -0,0 +1,57 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.HashMap;
+
+public class VG {
+    private JTextArea encrypttext;
+    private JButton 加密Button;
+    private JTextField origintext;
+    private JPanel vgpanel;
+    private JTextField keytext;
+    static  HashMap<Character,Integer> map=new HashMap<>();
+    static  char m[]=new char[26];
+
+    public VG() {
+        加密Button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String origin = origintext.getText().toLowerCase();
+                origin=origin.replace(" ","");
+                String key = keytext.getText().toLowerCase();
+                int l=origin.length();
+                while(key.length()<l)
+                    key=key+key;
+                StringBuilder encrypted = new StringBuilder(origin);
+                for(int i=0;i<l;++i)
+                {
+                    encrypted.replace(i,i+1,String.valueOf(m[(origin.charAt(i)-'a'+(key.charAt(i))-'a')%26]));
+                }
+                for(int i=0;i<l;++i){
+                    if(i!=0&&i%30==0){
+                        encrypted=encrypted.insert(i,'\n');
+                    }
+                }
+                encrypttext.setText(String.valueOf(encrypted));
+
+            }
+        });
+    }
+
+    public static void main(String[] args) throws ClassNotFoundException, UnsupportedLookAndFeelException, InstantiationException, IllegalAccessException {
+        UIManager.setLookAndFeel(javax.swing.plaf.nimbus.NimbusLookAndFeel.class.getName());
+        JFrame frame = new JFrame("古典加密算法_1617440518");
+        frame.setContentPane(new VG().vgpanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+        frame.setSize(520,450);
+        frame.setLocationRelativeTo(null);
+        for(int i=0;i<=25;++i){
+            m[i]= (char) ('a'+i);
+            map.put((char) ('a'+i),i);
+        }
+
+
+    }
+}
